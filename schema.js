313ccf344b213d92dbd2e1825948cb4fcf4b2fbc
evandikt <evandikt@gmail.com>
2020-11-09 14:27:33 +0100

    WIP

--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const schema = Joi.object({
   host: Joi.string().hostname().required(),
   port: Joi.number().port().required(),
-  password: Joi.string(),
+  password: Joi.string().allow('', null),
 })
 
 module.exports = schema
